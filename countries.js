e9f6ebca7def6a772c0af9314be533010e6bddf8
Marlene Gallardo <marleneg50346@students.laalliance.org>
2017-11-17 17:40:35 +0000

    Done.

---
+++ countries.js
@@ -0,0 +1,15 @@
+countries = [
+"chad", "cuba", "greenland", "iraq",
+"mali", "oman", "india"
+]
+countries.push("fiji")
+countries.splice(4, 0, "iran")
+countries.splice(7, 1)
+countries.reverse()
+countries.splice(5, 1, "togo")
+countries.push("laos")
+countries.reverse()
+countries.unshift("peru")
+console.log(countries)
+
+
